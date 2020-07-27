8bb9c9f17e66f6f98f1ddf1177c9180fd577e698
SoyeTrivan <68832330+SoyeTrivan@users.noreply.github.com>
2020-07-28 09:14:04 +1000

    API related changes

---
+++ DiscordDOJ/DiscordDOJ/Modules/Commands.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoyeAPI;
+
+namespace DiscordDOJ.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("players")]
+        public async Task Players()
+        {
+            string textFile = Directory.GetCurrentDirectory() + @"\config\players.txt";
+            string text = File.ReadAllText(textFile);
+
+            await ReplyAsync(text);
+        }
+    }
+}
---
+++ ServerDOJ/ServerDOJ/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using SoyeAPI;
+
+namespace ServerDOJ
+{
+    public class DOJScriptServer : BaseScript
+    {
+        public DOJScriptServer()
+        {
+            Tick += OnTick;
+        }
+
+        private async Task OnTick()
+        {
+            int playerCount = GetNumPlayerIndices();
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(DOJScriptHelper.ScriptLocation(), @"config\players.txt"), false))
+            {
+                outputFile.WriteLine(playerCount);
+            }
+            await Delay(10000);
+        }
+    }
+}
