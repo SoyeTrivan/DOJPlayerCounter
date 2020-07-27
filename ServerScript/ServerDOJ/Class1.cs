aa51a006faaab9d0beec39ba371d0a0a1180d13e
SoyeTrivan <68832330+SoyeTrivan@users.noreply.github.com>
2020-07-27 11:46:27 +1000

    Initial commit

---
+++ ServerScript/ServerDOJ/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using SoyeAPI;
+
+namespace ServerDOJ
+{
+    public class DOJScriptServer : BaseScript
+    {
+        public DOJScriptServer()
+        {
+            Tick += OnTick;
+        }
+
+        private async Task OnTick()
+        {
+            int playerCount = GetNumPlayerIndices();
+
+            string filePath = DOJScriptHelper.ScriptLocation() + "__resource.lua";
+            string docPath = Path.GetDirectoryName(filePath);
+
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "players.txt"), false))
+            {
+                outputFile.WriteLine(playerCount);
+            }
+            Console.WriteLine(playerCount);
+            await Delay(10000);
+        }
+    }
+}
